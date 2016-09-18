0a33bd945762fb6421841096ae11e2e2a7336ae8
Kevin Perkins <kevin.perkins@vice.com>
2016-09-17 22:30:11 -0400

    feat: implement control from 2-axis joystick

--- examples/gamepad/index.js
+++ examples/gamepad/index.js
@@ -3,6 +3,13 @@ var roboclaw = require("../../index").RoboClaw(0x80);
 // gamepad package for reading gamepad controller
 var gamepad = require("gamepad");
 
+var SpeedM1 = 0;
+var SpeedM2 = 0;
+var AxisX = 0;
+var AxisY = 0;
+var MAX_SPEED = 5000;
+var MIN_SPEED_THRESHOLD = 400;
+
 /*
   The following script will connect to a RoboClaw 2x15A Motor Controller,
   drive two motors, forward and backward, before halting and disconnecting.
@@ -28,13 +35,14 @@ setInterval(gamepad.processEvents, 100);
 setInterval(gamepad.detectDevices, 5000);
 // Listen for move events on all gamepads
 gamepad.on("move", function (id, axis, value) {
-  if (axis === 1) {
-    var speed = 5000 * -value;
+  if (axis === 0) AxisX = value;
+  if (axis === 1) AxisY = -value;
 
-    speed = (Math.abs(speed) < 400) ? 0 : speed;
-    // dual motor drive command with speed values; requires motor encoders
-    roboclaw.drive(speed, speed);
-  }
+  // calculate motor speeds based on gamepad axes positions
+  calculateMotorSpeeds();
+
+  // apply motor speeds to RoboClaw with drive command
+  setMotorSpeeds();
 });
 
 // close roboclaw and application, when any button pushed
@@ -47,3 +55,15 @@ gamepad.on("down", function (id, num) {
     process.exit();
   }, 250);
 });
+
+var calculateMotorSpeeds = function() {
+  SpeedM1 = ((AxisY + AxisX) / 2) * MAX_SPEED;
+  SpeedM2 = ((AxisY + (-AxisX)) / 2) * MAX_SPEED;
+
+  if (Math.abs(SpeedM1) < MIN_SPEED_THRESHOLD) SpeedM1 = 0;
+  if (Math.abs(SpeedM2) < MIN_SPEED_THRESHOLD) SpeedM2 = 0;
+};
+
+var setMotorSpeeds = function() {
+  roboclaw.drive(SpeedM1, SpeedM2);
+};
